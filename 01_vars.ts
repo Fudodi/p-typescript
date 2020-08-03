d5bf828d5ce329a0c34f7ed14624ce4e141a6999
Kisato <kisato.sakurai@nijibox.co.jp>
2020-08-03 11:15:13 +0900

    01_variables

---
+++ 01_vars.ts
@@ -0,0 +1,22 @@
+var n : Number = 1;
+var hoge : any = "robb";
+var moge : String = "an";
+var isDone : boolean = true;
+
+var nameing: string[] = ["john", "arya", "shin"];
+var names: any[] = ["john", "arya", "shin"];
+
+enum Starks { jon, bran, edd, cathy};
+
+var cat : Starks = Starks.cathy;
+
+function getName() : string {
+	// this defines the return value type
+	// if num, throw err
+	return "br";
+}
+
+function clgwin() : void {
+	console.log("winter is coming!");
+}
+
